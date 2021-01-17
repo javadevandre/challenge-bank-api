95b3d1de96ce1001592dd572eecc5b960f755ea5
André Luiz Cirino dos Santos <javadev.andre@gmail.com>
2021-01-16 23:21:41 -0300

    Base project

---
+++ src/main/java/api/bank/challenge/ChallengeBankApiApplication.java
@@ -0,0 +1,13 @@
+package api.bank.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeBankApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeBankApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api/bank/challenge/ChallengeBankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package api.bank.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeBankApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
